d5de62ac2ed78a16d5c6817484bafb50ff6dbfaa
unknown <acarles@jaumebalmes.net>
2023-03-21 12:13:34 +0100

    portal

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Stock Portal',
+    'depends': ['portal',
+                'base',
+                'website',
+                'web',
+                'stock',
+                'peliculas'
+                ],
+    'data':['views/template1.xml',
+            ],
+    
+    
+}
